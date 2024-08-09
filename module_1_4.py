8bf997cfc391edebb0387169e1c2d0354a134ad9
pipin <Aleksejsoronov3@gmail.com>
2024-08-09 13:18:12 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Как тебя зовут? ")
+print(len(my_string))
+print("Как тебя зовут?".upper())
+print("Как тебя зовут?".lower())
+print("Как тебя зовут?".replace(" ", "#"))
+print(my_string[0:-6])
+print(my_string[0 - 1])
+# Как тебя зовут?: Алексей
